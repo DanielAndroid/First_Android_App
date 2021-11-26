f1d541a0a545482ec1e4765a337a3aa99dee4786
DanielAndroid <humbleson32@gmail.com>
2021-11-26 16:59:58 -0400

    A simple app to help in learning android development.

---
+++ app/src/main/java/com/example/firstandroidapp/ui/SecondActivty.kt
@@ -0,0 +1,12 @@
+package com.example.firstandroidapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firstandroidapp.R
+
+class SecondActivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_activty)
+    }
+}
